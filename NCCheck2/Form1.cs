c7e90dcd9b21324d355725dab7f9e9755715e637
Khoa Bui <bmkhoa@gmail.com>
2016-08-21 05:31:46 +0700

    read file.

--- NCCheck2/Form1.cs
+++ NCCheck2/Form1.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,12 +19,44 @@ namespace NCCheck2
 
       private void openFile_Click(object sender, EventArgs e)
       {
+         // Create an instance of the open file dialog box.
+         OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
+         // Set filter options and filter index.
+         openFileDialog1.Filter = "CNC Files (.NC)|*.NC";
+         openFileDialog1.FilterIndex = 1;
+         openFileDialog1.Multiselect = false;
+
+         // Call the ShowDialog method to show the dialog box.
+         DialogResult result = openFileDialog1.ShowDialog();
+
+         // Process input if the user clicked OK.
+         if (DialogResult.OK.Equals(result))
+         {
+            // Open the selected file to read.
+            Stream fileStream = openFileDialog1.OpenFile();
+
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+               while (reader.Peek() >= 0)
+               {
+                  String processedLine = NCService.checkLine(reader.ReadLine());
+                  showLine(processedLine);
+               }
+            }
+            fileStream.Close();
+         }
       }
 
       private void checkFile_Click(object sender, EventArgs e)
       {
 
+      }
+
+      private void showLine(String line)
+      {
+         m_rtNC.AppendText(line);
+         m_rtNC.AppendText(Environment.NewLine);
       }
    }
 }
---
+++ NCCheck2/service/NCService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCheck2
+{
+   class NCService
+   {
+      public static String checkLine(String line)
+      {
+         return line;
+      }
+   }
+}
